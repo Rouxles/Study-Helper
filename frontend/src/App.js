6cacc7fc8613db4986ef18efa3ea774632cbe0ad
Vincent Wong <TheBrutux168@gmail.com>
2020-04-08 18:15:39 +1000

    Add initial flashcard interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card} from 'react-bootstrap'
-
+import CardList from "./components/CardList"
 
 function App() {
   return (
-    <Card style={{ width: '50rem', margin: 'auto', marginTop: '2rem'}}>
-      <Card.Body>
-        <Card.Title>Topic</Card.Title>
-        <Card.Text>
-          Question
-        </Card.Text>
-        <Card.Link href="#">Flip</Card.Link>
-      </Card.Body>
-      <Card.Footer className="text-muted">Large topic > Sub topic > Cur topic</Card.Footer>
-    </Card>
+    <CardList/>
   );
 }
 
---
+++ frontend/src/components/CardList.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {Card} from "react-bootstrap";
+import {Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const API_URL = "http://0.0.0.0:8000"
+
+export default class CardList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rawData: null,
+            cards: null
+        }
+    }
+
+    generateCards() {
+        const cards = [];
+
+        for (let majorTopic of this.state.rawData) {
+            const majorTopicTitle = majorTopic.major_topic;
+            for (let subTopic of majorTopic.sub_topics) {
+                const subTopicTitle = subTopic.sub_topic;
+                for (let question of subTopic.questions) {
+                    cards.push({
+                        majorTopic: majorTopicTitle,
+                        subTopic: subTopicTitle,
+                        question: question.question,
+                        answer: question.answer
+                    })
+                }
+            }
+        }
+
+        return cards;
+    }
+
+    componentDidMount() {
+        fetch(`${API_URL}/get_structured_cards`)
+            .then(res => res.json())
+            .then(data => this.setState({rawData: data.data}))
+    }
+
+    render() {
+        if (this.state.rawData) {
+            return (
+                <div>
+                    {this.generateCards().map(card => (
+                        <Card style={{width: '30rem', margin: "auto", marginTop: "1vh"}}>
+                            <Card.Header>{card.majorTopic}</Card.Header>
+                            <Card.Body>
+                                <Card.Title>{card.subTopic}</Card.Title>
+                                <Card.Text>{card.question}</Card.Text>
+                                <Button variant="primary">Reveal answer</Button>
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </div>
+            )
+        } else {
+            return null;
+        }
+    }
+}
