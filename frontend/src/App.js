db3015118104b0d838b36d7e0d5c6004082337dd
Vincent Wong <TheBrutux168@gmail.com>
2020-04-09 22:33:20 +1000

    Add navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav} from "react-bootstrap";
 import CardList from "./components/CardList"
 import Quiz from "./components/Quiz"
 
 function App() {
   return (
-    <Quiz/>
+    <div>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand>Study Helper</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link>Quiz Mode</Nav.Link>
+                <Nav.Link>Flashcard Collection</Nav.Link>
+                <Nav.Link>Flashcard Source</Nav.Link>
+                <Nav.Link>Flashcard Statistics</Nav.Link>
+            </Nav>
+        </Navbar>
+        <Quiz/>
+    </div>
   );
 }
 
